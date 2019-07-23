9cc6a129f5bd546839b9685dd02e376c23e2fd84
ofirn21-meet <ofirn21@meet.mit.edu>
2019-07-23 12:12:43 +0300

    Stopping here

---
+++ lab_3-greeting.py
@@ -0,0 +1,8 @@
+name=(input("what is your name?"))
+name=(name.capitalize())
+print("your name is "+str(len(name))+" letters long")
+print("the first letter of your name is "+(name[0].upper())+" and the last letter is "+name[-1].upper())
+print(name[1:-1]) 
+print("hello there,"+name)
+
+
